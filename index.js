ca94b43f124cd4bff11a75613359a7da7cc6cbd2
Mike Robards <mikerobards@icloud.com>
2021-04-08 16:49:44 -0400

    add js dot, wall, pp

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const width = 28
 const grid = document.querySelector('.grid')
 const scoreDisplay = document.querySelector('#score')
+let squares = []
 
-//28 * 28 = 784
+  //28 * 28 = 784
   // 0 - pac-dots
   // 1 - wall
   // 2 - ghost-lair
@@ -39,3 +40,29 @@ const layout = [
     1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
     1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1
 ]
+
+
+// create board
+function createBoard() {
+  for (let i=0;i<layout.length;i++) {
+    // create square
+    const square = document.createElement('div')
+    // put square in grid
+    grid.appendChild(square)
+    // put square in squares array
+    squares.push(square)
+    if (layout[i] === 0) {
+      squares[i].classList.add('pac-dot')
+    } else if (layout[i] === 1) {
+      squares[i].classList.add('wall')
+    } else if (layout[i] === 3) {
+      squares[i].classList.add('power-pellet')
+    }
+    
+
+
+  } 
+}
+
+createBoard()
+
