4e0373a5f2a6610ee2b27ddde4cd811bfda67c80
Mike Robards <mikerobards@icloud.com>
2021-04-23 15:09:53 -0400

    add ghosts

--- index.js
+++ index.js
@@ -136,3 +136,20 @@ function pacDotEaten() {
     squares[pacmanCurrentIndex].classList.remove('pac-dot')
   }
 }
+
+class Ghost {
+  constructor(className, startIndex, speed) {
+    this.className = className
+    this.startIndex = startIndex
+    this.speed = speed
+  }
+}
+
+const ghosts = [
+  new Ghost('blinky', 348, 250),
+  new Ghost('pinky', 376, 400),
+  new Ghost('inky', 351, 300),
+  new Ghost('clyde', 379, 500),
+]
+
+ghosts.forEach(ghost => squares[ghost.startIndex].classList.add(ghost.className)) 
